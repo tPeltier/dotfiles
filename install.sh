61c491a7514964eb0118741bc18ade8f19533116
tPeltier <81177526+tPeltier@users.noreply.github.com>
2022-05-25 21:12:02 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,24 +4,24 @@
 # nvim #
 ########
 
-mkdir -p "$XDG_CONFIG_HOME/nvim"
-mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
+#mkdir -p "$XDG_CONFIG_HOME/nvim"
+#mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 
-ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
+#ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 
 rm -rf "$XDG_CONFIG_HOME/X11"
 ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 
 # install neovim plugin manager
-[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
-    && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#[ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
+ #   && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
+  #  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
-ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
+#mkdir -p "$XDG_CONFIG_HOME/nvim/autoload"
+#ln -sf "$DOTFILES/nvim/autoload/plug.vim" "$XDG_CONFIG_HOME/nvim/autoload/plug.vim"
 
 # Install (or update) all the plugins
-nvim --noplugin +PlugUpdate +qa
+# nvim --noplugin +PlugUpdate +qa
 
 ######
 # i3 #
